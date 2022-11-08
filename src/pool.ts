a9cb6e81bb41842a5a2713c8377a828ce16d209f
0xhelloweb3 <1245816264@qq.com>
2022-11-08 18:47:10 +0800

    feat: get price

--- src/pool.ts
+++ src/pool.ts
@@ -1,5 +1,10 @@
 import { getObjectId,JsonRpcProvider, getObjectFields } from '@mysten/sui.js';
 
+/// Current fee is 0.3%
+const FEE_MULTIPLIER: bigint = 30n;
+    /// The integer scaling setting for fees calculation.
+const FEE_SCALE: bigint = 10000n;
+
 export interface PoolInfo {
     objectId: string,
     global: string,
@@ -19,7 +24,7 @@ export class Pool {
    async getPoolList():Promise<void>{  
    }
 
-   async getPoolDetail(poolAddress:string): Promise<PoolInfo> {
+   async getPoolInfo(poolAddress:string): Promise<PoolInfo> {
         const moveObject = await this.provider.getObject(poolAddress);
         const id = getObjectId(moveObject);
         const fields = getObjectFields(moveObject);
@@ -32,7 +37,23 @@ export class Pool {
             lpType: lpSupply?.type!,
             lpValue: lpSupply?.fields['value']
         }
-        return  Promise.resolve(poolInfo);
+        return Promise.resolve(poolInfo);
+   }
+
+   async getPrice(poolAddress:string, coinIn:bigint):Promise<bigint> {
+        let poolInfo = await this.getPoolInfo(poolAddress);
+        let reserveIn = poolInfo.tokenX;
+        let reserveOut = poolInfo.tokenY;
+        //let lpSupply = poolInfo.lpValue;
+
+        let fee_multiplier = FEE_SCALE - FEE_MULTIPLIER;
+
+        let coin_in_val_after_fees = coinIn * fee_multiplier;
+        // reserve_in size after adding coin_in (scaled to 1000)
+        let new_reserve_in = (reserveIn * FEE_SCALE) + coin_in_val_after_fees;
+
+        const amounOut = coin_in_val_after_fees * reserveOut / new_reserve_in;
+        return Promise.resolve(amounOut);
    }
 }
 
