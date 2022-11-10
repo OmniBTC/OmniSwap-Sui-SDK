7d78da2aeda141c2cfe2d0057918baa616fccb31
0xhelloweb3 <1245816264@qq.com>
2022-11-10 14:46:50 +0800

    feat: 0.0.7

--- src/coins/CoinsList.ts
+++ src/coins/CoinsList.ts
@@ -2,43 +2,44 @@ import { RawCoinInfo } from "./types";
 
 export const REQUESTS_MAINNET: RawCoinInfo[] = [
 
-    {
-      "name": "Tether USD",
-      "symbol": "USDT",
-      "official_symbol": "USDT",
-      "coingecko_id": "tether",
-      "decimals": 8,
-      "logo_url": "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/USDT.svg",
-      "project_url": "",
-      "token_type": {
-        "type": "0xbf2972612002f472b5bd21394b4417d75c9fe887::usdt::USDT",
-        "account_address": "0xbf2972612002f472b5bd21394b4417d75c9fe887",
-        "module_name": "usdt",
-        "struct_name": "USDT"
-      },
-      "extensions": {
-        "data": []
-      }
+  {
+    "name": "Tether USD",
+    "symbol": "USDT",
+    "official_symbol": "USDT",
+    "coingecko_id": "tether",
+    "decimals": 8,
+    "logo_url": "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/USDT.svg",
+    "project_url": "",
+    "token_type": {
+      "type": "0x6674cb08a6ef2a155b3c341a8697572898f0e4d1::usdt::USDT",
+      "account_address": "0x6674cb08a6ef2a155b3c341a8697572898f0e4d1",
+      "module_name": "usdt",
+      "struct_name": "USDT"
     },
-   
-    {
-      "name": "XBTC",
-      "symbol": "XBTC",
-      "official_symbol": "XBTC",
-      "coingecko_id": "",
-      "decimals": 8,
-      "logo_url": "https://coming-website.s3.us-east-2.amazonaws.com/icon_xbtc_30.png",
-      "project_url": "https://github.com/OmniBTC/OmniBridge",
-      "token_type": {
-        "type": "0xbf2972612002f472b5bd21394b4417d75c9fe887::xbtc::XBTC",
-        "account_address": "0xbf2972612002f472b5bd21394b4417d75c9fe887",
-        "module_name": "xbtc",
-        "struct_name": "XBTC"
-      },
-      "extensions": {
-        "data": []
-      }
+    "extensions": {
+      "data": []
+    }
+  },
+ 
+  {
+    "name": "XBTC",
+    "symbol": "XBTC",
+    "official_symbol": "XBTC",
+    "coingecko_id": "",
+    "decimals": 8,
+    "logo_url": "https://coming-website.s3.us-east-2.amazonaws.com/icon_xbtc_30.png",
+    "project_url": "https://github.com/OmniBTC/OmniBridge",
+    "token_type": {
+      "type": "0x6674cb08a6ef2a155b3c341a8697572898f0e4d1::xbtc::XBTC",
+      "account_address": "0x6674cb08a6ef2a155b3c341a8697572898f0e4d1",
+      "module_name": "xbtc",
+      "struct_name": "XBTC"
     },
+    "extensions": {
+      "data": []
+    }
+  },
+
     {
       "name": "Sui Coin",
       "symbol": "SUI",
