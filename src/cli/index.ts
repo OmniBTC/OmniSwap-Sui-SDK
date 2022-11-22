dbd045076d6ce4b986627ce812c23b3ef72b010a
0xhelloweb3 <1245816264@qq.com>
2022-11-22 12:41:34 +0800

    feat: add liquid

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,10 +1,12 @@
 
 import { faucetTokenCmd } from './faucetCmd'
 import { initProgram } from './option';
-import { walletCmd } from './walletCmd'
+import { walletCmd } from './walletCmd';
+import { addLiquidCmd } from './liquidCmd';
 
 const program = initProgram();
 faucetTokenCmd(program);
 walletCmd(program);
+addLiquidCmd(program);
 
 program.parse();
