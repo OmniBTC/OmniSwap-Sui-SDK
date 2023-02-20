2e017a8520ac2461e92d3c869518e03e23aee921
0xhelloweb3 <1245816264@qq.com>
2023-02-20 11:21:42 +0800

    feat: add devnet

--- src/coins/CoinsList.ts
+++ src/coins/CoinsList.ts
@@ -172,3 +172,116 @@ export const REQUESTS_TESTNET: RawCoinInfo[] = [
       }
     },
 ]
+
+export const REQUESTS_DEVNET: RawCoinInfo[] = [
+
+  {
+    "name": "Tether USD",
+    "symbol": "USDT",
+    "official_symbol": "USDT",
+    "coingecko_id": "tether",
+    "decimals": 8,
+    "logo_url": "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/USDT.svg",
+    "project_url": "",
+    "token_type": {
+      "type": "0x7263b90384c15e1bffe5757d9eaa0235264bd294::coins::USDT",
+      "account_address": "0x7263b90384c15e1bffe5757d9eaa0235264bd294",
+      "module_name": "usdt",
+      "struct_name": "USDT"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+  {
+    "name": "XBTC",
+    "symbol": "XBTC",
+    "official_symbol": "XBTC",
+    "coingecko_id": "",
+    "decimals": 8,
+    "logo_url": "https://coming-website.s3.us-east-2.amazonaws.com/icon_xbtc_30.png",
+    "project_url": "https://github.com/OmniBTC/OmniBridge",
+    "token_type": {
+      "type": "0x7263b90384c15e1bffe5757d9eaa0235264bd294::coins::XBTC",
+      "account_address": "0x7263b90384c15e1bffe5757d9eaa0235264bd294",
+      "module_name": "xbtc",
+      "struct_name": "XBTC"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+  {
+    "name": "Sui Coin",
+    "symbol": "SUI",
+    "official_symbol": "SUI",
+    "coingecko_id": "Sui",
+    "decimals": 9,
+    "logo_url": "https://raw.githubusercontent.com/MystenLabs/sui/main/apps/wallet/src/ui/assets/images/sui-icon.png",
+    "project_url": "http://sui.io/",
+    "token_type": {
+      "type": "0x2::sui::SUI",
+      "account_address": "0x2",
+      "module_name": "sui",
+      "struct_name": "SUI"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+  {
+    "name": "BTC",
+    "symbol": "BTC",
+    "official_symbol": "BTC",
+    "coingecko_id": "",
+    "decimals": 8,
+    "logo_url": "https://coming-website.s3.us-east-2.amazonaws.com/icon_xbtc_30.png",
+    "project_url": "https://github.com/OmniBTC/OmniBridge",
+    "token_type": {
+      "type": "0x7263b90384c15e1bffe5757d9eaa0235264bd294::coins::BTC",
+      "account_address": "0x7263b90384c15e1bffe5757d9eaa0235264bd294",
+      "module_name": "btc",
+      "struct_name": "BTC"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+  {
+    "name": "Binance Coin",
+    "symbol": "BNB",
+    "official_symbol": "BNB",
+    "coingecko_id": "binancecoin",
+    "decimals": 8,
+    "logo_url": "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/BNB.svg",
+    "project_url": "",
+    "token_type": {
+      "type": "0x7263b90384c15e1bffe5757d9eaa0235264bd294::coins::BNB",
+      "account_address": "0x7263b90384c15e1bffe5757d9eaa0235264bd294",
+      "module_name": "bnb",
+      "struct_name": "BNB"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+
+  {
+    "name": "Ethereum",
+    "symbol": "ETH",
+    "official_symbol": "ETH",
+    "coingecko_id": "eth",
+    "decimals": 6,
+    "logo_url": "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/WETH.svg",
+    "project_url": "",
+    "token_type": {
+      "type": "0x7263b90384c15e1bffe5757d9eaa0235264bd294::coins::ETH",
+      "account_address": "0x7263b90384c15e1bffe5757d9eaa0235264bd294",
+      "module_name": "eth",
+      "struct_name": "ETH"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+]
--- src/modules/CoinListModule.ts
+++ src/modules/CoinListModule.ts
@@ -1,6 +1,6 @@
 import { IModule } from '../interfaces/IModule'
 import { SDK } from '../sdk';
-import { REQUESTS_MAINNET,REQUESTS_TESTNET, RawCoinInfo} from '../coins'
+import { REQUESTS_MAINNET,REQUESTS_TESTNET,REQUESTS_DEVNET, RawCoinInfo} from '../coins';
 
 export class CoinListModule implements IModule {
     protected _sdk: SDK;
@@ -11,7 +11,9 @@ export class CoinListModule implements IModule {
     
     constructor(sdk: SDK) {
       this._sdk = sdk;
-      this.coinList = sdk.networkOptions.isMainNet ? REQUESTS_MAINNET: REQUESTS_TESTNET;
+      const networkName = sdk.networkOptions.name;
+      this.coinList = networkName === 'devnet' ? REQUESTS_DEVNET :
+               (networkName === 'testnet' ? REQUESTS_TESTNET : REQUESTS_MAINNET);
       this.fullnameToCoinInfo = {};
       this.symbolToCoinInfo = {};
       this.typeToCoinInfo = {};
